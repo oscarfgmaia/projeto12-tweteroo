02c2dbbda176231d235dcea5859cb4a61dcf32bb
Oscar Maia <oscarfgmaia@gmail.com>
2022-11-04 22:55:39 -0300

    messages on tweeter

--- index.js
+++ index.js
@@ -6,67 +6,107 @@ app.use(cors());
 app.use(express.json());
 const users = [
   {
-    username: "luffy",
+    username: "Luffy",
     avatar:
       "https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/05/luffy-one-piece.webp?fit=1200%2C675&ssl=1",
   },
   {
-    username: "sanji",
+    username: "Sanji",
     avatar:
       "https://criticalhits.com.br/wp-content/uploads/2021/10/DXSH4n_XUAAigjE-1-910x512.jpg",
   },
   {
-    username: "zoro",
+    username: "Zoro",
     avatar:
       "https://ovicio.com.br/wp-content/uploads/2021/07/20210712-one-piece-zoro-wano-postcover-555x555.jpg",
   },
   {
-    username: "nami",
+    username: "Nami",
     avatar:
       "https://animes.olanerd.com/wp-content/uploads/2022/09/1662502357_One-Piece-Nami-tem-Haki.jpg",
   },
   {
-    username: "shanks",
-    avatar:
-      "https://criticalhits.com.br/wp-content/uploads/2022/07/Shanks-910x455.jpg",
-  },
-  {
-    username: "usopp",
+    username: "Usopp",
     avatar:
       "https://i.pinimg.com/736x/51/80/9d/51809d19d36ea87335edde7f74e982ae.jpg",
+  },
+
+  {
+    username: "Chopper",
+    avatar:
+      "https://i.pinimg.com/originals/89/ff/79/89ff79195daf1da1b138b18d0cc63304.jpg",
+  },
+  {
+    username:"Franky",
+    avatar:"https://i.pinimg.com/originals/62/81/74/628174766c32cf1c1afe53041cbf8bf5.jpg"
+  },
+  {
+    username:"Brook",
+    avatar:"https://criticalhits.com.br/?attachment_id=208749",
+  },
+  {
+    username:"Robin",
+    avatar:"https://nerdhits.com.br/wp-content/uploads/2022/07/nico-robin-one-piece-1.jpg",
   },
 ];
 
 const tweets = [
   {
-    username: "luffy",
+    username: "Luffy",
     avatar:
       "https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/05/luffy-one-piece.webp?fit=1200%2C675&ssl=1",
     tweet: "Eu vou ser o rei dos piratas!",
   },
   {
-    username: "nami",
+    username: "Nami",
     avatar:
       "https://animes.olanerd.com/wp-content/uploads/2022/09/1662502357_One-Piece-Nami-tem-Haki.jpg",
     tweet: "Odeio quando não pagam o que me deve!",
   },
   {
-    username: "shanks",
-    avatar:
-    "https://criticalhits.com.br/wp-content/uploads/2022/07/Shanks-910x455.jpg",
-    tweet: "...",
-  },
-  {
-    username: "luffy",
+    username: "Luffy",
     avatar:
       "https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2022/05/luffy-one-piece.webp?fit=1200%2C675&ssl=1",
     tweet: "Sanjiiiii quero carne!!!!",
   },
   {
-    username: "sanji",
+    username: "Usopp",
     avatar:
-      "https://criticalhits.com.br/wp-content/uploads/2021/10/DXSH4n_XUAAigjE-1-910x512.jpg",
-    tweet:"Só depois que eu terminar a sobremesa das meninas!"
+      "https://i.pinimg.com/736x/51/80/9d/51809d19d36ea87335edde7f74e982ae.jpg",
+    tweet:"Eu não erro nenhum tiro!"
+  },
+  {
+    username: "Sanji",
+    avatar:
+    "https://criticalhits.com.br/wp-content/uploads/2021/10/DXSH4n_XUAAigjE-1-910x512.jpg",
+    tweet: "Só depois que eu terminar a sobremesa das meninas!",
+  },
+  {
+    username: "Zoro",
+    avatar:
+      "https://ovicio.com.br/wp-content/uploads/2021/07/20210712-one-piece-zoro-wano-postcover-555x555.jpg",
+      tweet:"Cala a boca seu cozinheiro de terceira"
+  },
+  {
+    username: "Chopper",
+    avatar:
+      "https://i.pinimg.com/originals/89/ff/79/89ff79195daf1da1b138b18d0cc63304.jpg",
+      tweet:"Eu não fiquei nenhum pouco feliz com isso!! Seu idiota!!"
+  },
+  {
+    username:"Franky",
+    avatar:"https://i.pinimg.com/originals/62/81/74/628174766c32cf1c1afe53041cbf8bf5.jpg",
+    tweet:"SUUUPEEEEER!!!!"
+  },
+  {
+    username:"Brook",
+    avatar:"https://criticalhits.com.br/?attachment_id=208749",
+    tweet:"Posso ver sua calcinha? :3"
+  },
+  {
+    username:"Robin",
+    avatar:"https://nerdhits.com.br/wp-content/uploads/2022/07/nico-robin-one-piece-1.jpg",
+    tweet:"hahahaha"
   },
 ];
 
@@ -97,23 +137,28 @@ app.post("/sign-up", (req, res) => {
 });
 
 app.post("/tweets", (req, res) => {
-  const { username, tweet } = req.body;
-  if (!username || !tweet) {
-    res.status(400).send("Todos os campos são obrigatórios");
-    return;
-  }
-  const user = users.find(
-    (obj) => obj.username.toLocaleLowerCase() === username.toLocaleLowerCase()
+  const {tweet} = req.body;
+  const {user} = req.headers;
+
+  const userLoggedIn = users.find(
+    (obj) => obj.username.toLocaleLowerCase() === user.toLocaleLowerCase()
   );
-  if (!user) {
+  console.log(userLoggedIn)
+  if (!userLoggedIn) {
     res.status(404).send("Usuário inválido");
     return;
   }
+  if(!userLoggedIn.username || !tweet){
+    res.status(400).send("Todos os campos são obrigatórios");
+    return;
+  }
+  
   const newTweet = {
-    username: username,
-    avatar: user.avatar,
+    username: userLoggedIn.username,
+    avatar: userLoggedIn.avatar,
     tweet,
   };
+
   tweets.push(newTweet);
   res.status(201).send("OK");
 });
@@ -125,6 +170,7 @@ app.get("/tweets", (req, res) => {
       break;
     }
     lastTenTweets.push(tweets[i]);
+
   }
   res.send(lastTenTweets);
 });
@@ -133,13 +179,13 @@ app.get("/tweets", (req, res) => {
 app.get("/tweets/:USERNAME", (req, res) => {
   const username = req.params.USERNAME.toLocaleLowerCase();
 
-  if (!users.find((obj) => obj.username === username)) {
+  if (!users.find((obj) => obj.username.toLocaleLowerCase() === username)) {
     res.status(404).send("Usuário não cadastrado!");
     return;
   }
 
   const tweetsByUser = tweets.filter((obj) => {
-    if (obj.username === username) {
+    if (obj.username.toLocaleLowerCase() === username) {
       return true;
     }
   });
